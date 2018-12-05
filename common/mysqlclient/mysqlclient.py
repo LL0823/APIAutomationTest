dc716fb03d0204983a088082544b5efe6603ee63
yanchunhuo <yanchunhuo@jw.hongshu.com>
2018-12-05 18:05:44 +0800

    修改mysqlclient

--- common/mysqlclient/mysqlclient.py
+++ common/mysqlclient/mysqlclient.py
@@ -1,22 +1,40 @@
 #-*- coding:utf-8 -*-
-#!/usr/bin/python
+
 import pymysql
-class MysqlClient(object):
+
+class MysqlClient():
     def __init__(self,host,port,username,password,dbname,charset='utf8'):
-        self._host=host
-        self._port=port
-        self._username=username
-        self._password=password
-        self._dbname=dbname
-        self._charset=charset
-        self._conn=pymysql.connect(host=self._host,port=int(self._port),user=self._username,passwd=self._password,db=self._dbname,charset=self._charset)
+        self._conn=pymysql.connect(host=host,port=int(port),user=username,passwd=password,db=dbname,charset=charset)
         self._cursor=self._conn.cursor()
 
     def executeSQL(self,sql):
+        # try:
+        # 为了避免连接被服务器关闭,检测进行重连
+        self._conn.ping(reconnect=True)
         self._cursor.execute(sql)
         result=self._cursor.fetchall()
         self._conn.commit()
+        # except:
+        #     self._conn.rollback()
         return result
+
+    def executeMany(self,query,values):
+        """
+        :param query: insert into table(field1,field2) values(%s,%s)
+        :param values: [(field1_value1,field2_value2),(field1_value3,field2_value4)]
+        :return:
+        """
+        # try:
+        # 为了避免连接被服务器关闭,检测进行重连
+        self._conn.ping(reconnect=True)
+        num=len(values)
+        n=0
+        while n<num:
+            self._cursor.executemany(query, values[n:n+1000])
+            self._conn.commit()
+            n+=1000
+        # except:
+        #     self._conn.rollback()
 
     def closeAll(self):
         self._cursor.close()
